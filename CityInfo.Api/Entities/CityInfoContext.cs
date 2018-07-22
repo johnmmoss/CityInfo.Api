9ebfc95452acf707c1e4dfae23e7b74291dbfd08
John Moss <john.moss@HST.HIGHSPEEDTRAINING.CO.UK>
2018-07-22 13:42:20 +0100

    Added in Ef and sql server

---
+++ CityInfo.Api/Entities/CityInfoContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.Api.Entities
+{
+    public class CityInfoContext : DbContext
+    {
+        public CityInfoContext(DbContextOptions<CityInfoContext> options)
+            : base(options)
+        {
+            // Force database
+            //Database.EnsureCreated();
+
+            Database.Migrate();
+        }
+
+        public DbSet<City> Cities { get; set; }
+
+        public DbSet<PointOfInterest> PointOfInterests { get; set; }
+
+        // One option for configuring - but can also pass to constructor!
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("connectionString");
+
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
